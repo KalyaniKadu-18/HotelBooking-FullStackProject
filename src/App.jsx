bb8a52bc38f0bd3a4b0c23fd98f08632c7c7a131
kalyaniKadu18 <kadukalyani30@gmail.com>
2025-06-03 14:34:39 -0500

    Implemented room details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Navbar from './Components/Navbar/Navbar';
 import { Routes, Route , useLocation } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import Footer from './Components/Footer/Footer';
 import Allrooms from './pages/Allrooms/Allrooms';
+import RoomDetails from './pages/roomDetails/roomDetails';
 
 const App = () => {
 
@@ -12,8 +13,9 @@ const isOwnerPath = useLocation().pathname.includes("owner");
     <div>
       {!isOwnerPath && <Navbar/> }
       <Routes>
-      <Route path='/' element={<Home/>} />
-            <Route path='/rooms' element={<Allrooms/>} />
+       <Route path='/' element={<Home/>} />
+       <Route path='/rooms' element={<Allrooms/>} />
+       <Route path='/rooms/:id' element={<RoomDetails/>} />
       </Routes>
       <Footer/>
     </div>
