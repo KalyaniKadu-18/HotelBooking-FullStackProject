3903bd055388af79c60193d7cccf8980b8e6616b
kalyaniKadu18 <kadukalyani30@gmail.com>
2025-06-04 15:31:45 -0500

    Implemented Mybookings page and Hotel registration form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home/Home';
 import Footer from './Components/Footer/Footer';
 import Allrooms from './pages/Allrooms/Allrooms';
 import RoomDetails from './pages/roomDetails/roomDetails';
+import MyBookings from './pages/MyBookings/MyBookings';
+import HotelReg from './Components/HotelReg/HotelReg';
 
 const App = () => {
 
@@ -12,10 +14,12 @@ const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
       {!isOwnerPath && <Navbar/> }
+      {false && <HotelReg/>}
       <Routes>
        <Route path='/' element={<Home/>} />
        <Route path='/rooms' element={<Allrooms/>} />
        <Route path='/rooms/:id' element={<RoomDetails/>} />
+        <Route path='/my-bookings' element={<MyBookings/>} />
       </Routes>
       <Footer/>
     </div>
