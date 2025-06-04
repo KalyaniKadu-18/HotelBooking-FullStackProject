648314db0fd1213dcd99f2456e3be1f964be7a75
kalyaniKadu18 <kadukalyani30@gmail.com>
2025-06-04 18:54:29 -0500

    Implemented Owner dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
-import Navbar from './Components/Navbar/Navbar';
-import { Routes, Route , useLocation } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Footer from './Components/Footer/Footer';
-import Allrooms from './pages/Allrooms/Allrooms';
-import RoomDetails from './pages/roomDetails/roomDetails';
-import MyBookings from './pages/MyBookings/MyBookings';
-import HotelReg from './Components/HotelReg/HotelReg';
+import Navbar from "./Components/Navbar/Navbar";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Footer from "./Components/Footer/Footer";
+import Allrooms from "./pages/Allrooms/Allrooms";
+import RoomDetails from "./pages/roomDetails/roomDetails";
+import MyBookings from "./pages/MyBookings/MyBookings";
+import HotelReg from "./Components/HotelReg/HotelReg";
+import Layout from "./pages/HotelOwner/Layout";
+import Dashboard from "./pages/HotelOwner/Dashboard";
+import AddRoom from "./pages/HotelOwner/AddRoom";
+import ListRoom from "./pages/HotelOwner/ListRoom";
 
 const App = () => {
-
-const isOwnerPath = useLocation().pathname.includes("owner");
+  const location = useLocation();
+  const isOwnerPath = location.pathname.includes("owner");
 
   return (
     <div>
-      {!isOwnerPath && <Navbar/> }
-      {false && <HotelReg/>}
+      {!isOwnerPath && <Navbar />}
+      {false && <HotelReg />}
       <Routes>
-       <Route path='/' element={<Home/>} />
-       <Route path='/rooms' element={<Allrooms/>} />
-       <Route path='/rooms/:id' element={<RoomDetails/>} />
-        <Route path='/my-bookings' element={<MyBookings/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms" element={<Allrooms />} />
+        <Route path="/rooms/:id" element={<RoomDetails />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/owner" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-room" element={<AddRoom />} />
+          <Route path="list-room" element={<ListRoom />} />
+        </Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
