14a6abdcb6c0f070809e10759783ad931540a102
kalyaniKadu18 <kadukalyani30@gmail.com>
2025-06-07 15:28:30 -0500

    Frontend code finalized and ready

--- src/Components/HomeComponents/FeaturedDestination/FeaturedDestination.jsx
+++ src/Components/HomeComponents/FeaturedDestination/FeaturedDestination.jsx
@@ -9,7 +9,7 @@ const FeaturedDestination = () => {
   return (
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20 '>
 
-    <Title title='Featured Destionation' subTitle='Discover our handpicked selection of exceptional properties around the world, 
+    <Title title='Featured Destionation' subTitle='Discover our handpicked selection of the exceptional properties around the world, 
     offering unparalleled luxury and unforgettable experiences.'/>
 
       <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
@@ -20,7 +20,7 @@ const FeaturedDestination = () => {
       <button 
       onClick={() => {navigate('/rooms'); scrollTo(0,0)}}
       className='my-16 px-4 py-2 text-sm font-medium  border border-gray-300  rounded 
-      bg-white hover:bg-gray-50 transition-all cursur-pointer'>View All Destinatins</button>
+      bg-white hover:bg-gray-50 transition-all cursur-pointer'>View All Destinations</button>
     </div>
   )
 }
